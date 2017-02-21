5c8cd28bea17a84497ad6fe101d4044af6f58d4d
bartek <mail@bartekpawlowski.pl>
2017-02-21 15:43:37 +0100

    Small changes in code - put all styles into style.xml (landscape mode), rearange buttons in landscape mode, add more space under team name in landscape mode.

--- app/src/main/java/pl/bartekpawlowski/handballcounter/MainActivity.java
+++ app/src/main/java/pl/bartekpawlowski/handballcounter/MainActivity.java
@@ -3,6 +3,7 @@ package pl.bartekpawlowski.handballcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
